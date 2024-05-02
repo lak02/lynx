427bbfe7a76d523ae1abd0bb279e59bb44137ea6
David Leng <davidleng0x0@foxmail.com>
2024-05-02 23:27:55 +0800

    chore: add dependencies&lint first

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,21 +2,19 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-foundry");
 /** @type import('hardhat/config').HardhatUserConfig */
 
-
 const COMPILER_SETTINGS = {
   optimizer: {
     enabled: true,
     runs: 1000000,
   },
-}
-
+};
 
 module.exports = {
   paths: {
     artifacts: "./hardhat-artifacts",
     cache: "./hardhat-cache",
-    "sources": "./src",
-    "tests": "./tests",
+    sources: "./src",
+    tests: "./tests",
   },
   solidity: {
     compilers: [
@@ -24,6 +22,6 @@ module.exports = {
         version: "0.8.25",
         settings: COMPILER_SETTINGS,
       },
-    ]
-  }
+    ],
+  },
 };
